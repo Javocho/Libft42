aeaabe8ae80bf57203ba8662cd2c3c2bc4bd61be
gevuit <jav8yt@gmail.com>
2023-05-16 19:44:06 +0200

    more or less ft_atoi adapted to 42 rules with an extremely good main

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,3 +1,5 @@
+#include "libft.h"
+
 int ft_atoi(const char *str)
 {
     int i;
@@ -20,3 +22,32 @@ int ft_atoi(const char *str)
     }
     return (result * sign);
 }
+
+// int main(void)
+// {
+//     const char *test_cases[] = {
+//         "42",
+//         "-123",
+//         "+987",
+//         "0",
+//         "   555",
+//         "  -999",
+//         "abc",
+//         "123abc",
+//         "  42abc",
+//         "- 123",
+//         "+456",
+//         "  - 789",
+//         NULL
+//     };
+
+//     int i = 0;
+//     while (test_cases[i] != NULL)
+//     {
+//         int result = ft_atoi(test_cases[i]);
+//         printf("Input: \"%s\", Output: %d\n", test_cases[i], result);
+//         i++;
+//     }
+
+//     return 0;
+// }
