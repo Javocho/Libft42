20bcf9b9f0e3aafed6e1e93c72a5a002ac08c794
Francisco Javier Costa Ferrer-Vidal <49961413+Javocho@users.noreply.github.com>
2023-05-16 19:21:58 +0200

    Delete 43-1/libft directory

--- 43-1/libft/ft_strcmp.c
+++
@@ -1,9 +0,0 @@
-int ft_strcmp(const char *str1, const char *str2)
-{
-    int i;
-
-    i = 0;
-    while (str1[i] && str2[i] && str1[i] == str2[i])
-        i++;
-    return ((unsigned char)str1[i] - (unsigned char)str2[i]);
-}
--- 43-1/libft/ft_toupper.c
+++
@@ -1,6 +0,0 @@
-int ft_toupeer(int c)
-{
-    if (c >= 'a' && c <= 'z')
-        return (c - 32);
-    return (c);
-}
