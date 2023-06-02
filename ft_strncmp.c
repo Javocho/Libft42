5bffdfa8f5bc8b8b5bdf013fc9672602c123cc59
gevuit <jav8yt@gmail.com>
2023-06-02 20:01:58 +0200

    strncmp fixed for special characters and with and more efficient

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: fcosta-f <fcosta-f@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/27 18:47:11 by fcosta-f          #+#    #+#             */
-/*   Updated: 2023/05/27 18:57:10 by fcosta-f         ###   ########.fr       */
+/*   Updated: 2023/06/02 20:00:45 by fcosta-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,29 @@
 
 int	ft_strncmp(const char *str1, const char *str2, size_t n)
 {
-	size_t	i;
+	size_t			i;
+	unsigned char	c1;
+	unsigned char	c2;
 
 	i = 0;
-	while (str1[i] && str2[i] && str1[i] == str2[i] && i < n)
+	c1 = str1[i];
+	c2 = str2[i];
+	while (i < n && c1 != '\0' && c2 != '\0' && c1 == c2)
+	{
 		i++;
+		c1 = str1[i];
+		c2 = str2[i];
+	}
 	if (i == n)
 		return (0);
-	return (str1[i] - str2[i]);
+	return (c1 - c2);
 }
 
 /*
 int main(void) {
-    const char *str1 = "Hello";
-    const char *str2 = "Hellp";
-    int result = ft_strncmp(str1, str2, 4);
+    const char *str1 = "test\200";
+    const char *str2 = "test\0";
+    int result = ft_strncmp(str1, str2, 10);
     
     printf("String 1: %s\n", str1);
     printf("String 2: %s\n", str2);
