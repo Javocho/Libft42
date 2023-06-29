f808fd0ffb65ab1c6b60e0d2c49fade7d6931e78
gevuit <jav8yt@gmail.com>
2023-06-29 16:39:27 +0200

    ft_itoa fixed

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,32 +6,50 @@
 /*   By: fcosta-f <fcosta-f@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 12:20:37 by fcosta-f          #+#    #+#             */
-/*   Updated: 2023/05/26 12:21:49 by fcosta-f         ###   ########.fr       */
+/*   Updated: 2023/06/29 16:38:19 by fcosta-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+int	count_digits(int n)
+{
+	int	digits;
+
+	digits = 0;
+	if (n == 0)
+		++digits;
+	if (n < 0)
+		++digits;
+	while (n != 0)
+	{
+		++digits;
+		n /= 10;
+	}
+	return (digits);
+}
+
 char	*ft_itoa(int n)
 {
 	char	*str;
 	int		digits;
-	int		num;
 
-	digits = 0;
-	num = n;
-	if (num < 0)
-		++digits;
-	while (num != 0)
-	{
-		++digits;
-		num /= 10;
-	}
+	digits = count_digits(n);
 	str = malloc(sizeof(char) * (digits + 1));
 	if (str == NULL)
 		return (NULL);
-	str[digits--] = '\0';
-	while (digits >= 0)
+	str[digits] = '\0';
+	--digits;
+	if (n == 0)
+		str[0] = '0';
+	else if (n == -2147483648)
+		str = "-2147483648";
+	else if (n < 0)
+	{
+		str[0] = '-';
+		n *= -1;
+	}
+	while (n > 0)
 	{
 		str[digits] = '0' + n % 10;
 		n /= 10;
@@ -40,10 +58,11 @@ char	*ft_itoa(int n)
 	return (str);
 }
 
-/*
-int main(void)
+/*#include <stdlib.h>
+#include <stdio.h>
+int main(int argc, char **argv)
 {
-	int num = -12345;
+	int num = ft_atoi(argv[1]);
 	char *str = ft_itoa(num);
 
 	if (str == NULL)
@@ -55,7 +74,7 @@ int main(void)
 	printf("Número: %d\n", num);
 	printf("Cadena resultante: %s\n", str);
 
-	free(str); /
+	free(str);
 
 	return 0;
 }
