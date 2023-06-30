e44e1c73a46dad5eaddfcb468ea466de4635bf5b
gevuit <jav8yt@gmail.com>
2023-06-30 11:50:34 +0200

    strnstr fixed and beautified

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: fcosta-f <fcosta-f@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/28 13:26:39 by fcosta-f          #+#    #+#             */
-/*   Updated: 2023/05/31 18:26:47 by fcosta-f         ###   ########.fr       */
+/*   Updated: 2023/06/30 11:50:00 by fcosta-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,34 +18,35 @@ char	*ft_strnstr(const char *str1, const char *str2, size_t n)
 	size_t	j;
 
 	i = 0;
-	j = 0;
-	while (str1[i] && str2[j] && i < n)
+	if (!*str2)
+		return ((char *)str1);
+	while (str1[i])
 	{
-		if (str1[i] == str2[j])
-			j++;
-		else
-			j = 0;
-		i++;
+		j = 0;
+		while (str1[i + j] == str2[j] && (size_t)(i + j) < n)
+		{
+			if (str2[j + 1] == '\0')
+				return ((char *)(str1 + i));
+			++j;
+		}
+		++i;
 	}
-	if (str2[j] == '\0')
-		return ((char *)(str1 + i - j));
 	return (NULL);
 }
 
-/*
-int main()
+/*#include <stdio.h>
+int main(int argc, char **argv)
 {
     const char *str1 = "Hello, world!";
     const char *str2 = "world";
-    size_t n = strlen(str1);
+    //size_t n = -1;
+	if (argc > 1) {
+		char *result = ft_strnstr(str1, str2, ft_atoi(argv[1]));
 
-    char *result = ft_strnstr(str1, str2, n);
-
-    if (result != NULL)
-        printf("Substring found at position: %ld\n", result - str1);
-    else
-        printf("Substring not found\n");
-
-    return 0;
-}
-*/
+		if (result != NULL)
+			printf("Substring found at position: %ld\n", result - str1);
+		else
+			printf("Substring not found\n");
+	}
+	return 0;
+}*/
