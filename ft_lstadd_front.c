f251da1603ff4ba24b3d17f0956674bde96d9c83
gevuit <jav8yt@gmail.com>
2023-07-01 17:14:28 +0200

    new functions bonus

---
+++ ft_lstadd_front.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fcosta-f <fcosta-f@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/01 15:54:30 by fcosta-f          #+#    #+#             */
+/*   Updated: 2023/07/01 17:14:10 by fcosta-f         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_front(t_list **lst, t_list *new)
+{
+	if (lst && new)
+	{
+		new->next = *lst;
+		*lst = new;
+	}
+}
+
+//argumento por copia y con doble puntero se puede modificar
