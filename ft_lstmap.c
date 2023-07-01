86266692f699a98cf2867e9acf659a0654cb5f4a
Francisco Javier Costa Ferrer-Vidal <fcosta-f@cbr10s2.42barcelona.com>
2023-07-01 20:43:23 +0200

    lstmap fixed

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: fcosta-f <fcosta-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/01 19:32:22 by fcosta-f          #+#    #+#             */
-/*   Updated: 2023/07/01 20:13:03 by fcosta-f         ###   ########.fr       */
+/*   Updated: 2023/07/01 20:43:02 by fcosta-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,21 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new;
-	t_list	*save;
-	int		b;
+	t_list	*first;
 
-	b = 1;
+	if (!f || !del)
+		return (NULL);
+	first = NULL;
 	while (lst)
 	{
 		new = ft_lstnew(f(lst->content));
-		if (b == 1)
-		{
-			b = 0;
-			save = new;
-		}
 		if (!new)
 		{
-			ft_lstclear(&save, del);
+			ft_lstclear(&first, del);
 			return (NULL);
 		}
-		new = new->next;
+		ft_lstadd_back(&first, new);
 		lst = lst->next;
 	}
-	new->next = NULL;
-	return (save);
+	return (first);
 }
